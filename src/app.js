ccde0832444bc6b2301d198159149fd5086a17d1
Ignacio Allue <ignacioallue@hotmail.com>
2023-08-15 13:23:02 -0300

    Realizando desafio 4

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import productRouter from './src/routes/product.js'
-import cartRouter from './src/routes/cart.js'
-import realTimeProducts from './src/routes/realtimeproducts.js'
+import productRouter from './routes/product.js'
+import cartRouter from './routes/cart.js'
+import realTimeProducts from './routes/realtimeproducts.js'
 import { Server } from 'socket.io'
-import { __dirname } from './src/utils/utils.js'
+import { __dirname } from './utils.js'
 import handlebars from 'express-handlebars'
 
 const app = express()
@@ -13,12 +13,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname+'public'))
 
 app.engine('handlebars', handlebars.engine())
-app.set('realtimeproducts',__dirname+'/realtimeproducts')
+app.set('views',__dirname+'/views')
 app.set('view engine', 'handlebars')
 
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter)
-app.use('/api/realtimeproducts', realTimeProducts)
+app.use('/realtimeproducts', realTimeProducts)
 
 const PORT = 8080
 const httpServer = app.listen(PORT, () => console.log(`Server running on port ${httpServer.address().port}`))
@@ -28,9 +28,9 @@ const socketServer = new Server(httpServer)
 
 const products = []
 
-socketServer.on('conection', socket => {
-    socket.on('product',infoProduct => {
-        products.push(infoProduct)
-        socketServer.emit('newProduct', products)
+socketServer.on('connection', socket => {
+    socket.on('producto',infoProduct => {
+        console.log(infoProduct)
+        socketServer.emit('newProducts', products)
     })
 })
