c5975387a8001193ac735d59462d5cdb45ecc475
Ignacio Allue <ignacioallue@hotmail.com>
2023-09-07 11:33:41 -0300

    Nuevo desafio

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import session from 'express-session'
 import router from './routes/products.router.js'
 import cartRouter from './routes/carts.router.js'
+import handlebars from 'handlebars'
 import { __dirname } from './utils.js'
 import './db/dbConfig.js'
-
+import MongoStore from 'connect-mongo'
+import mongoose from 'mongoose'
+import sessionRouter from './routes/sessions.router.js'
+import viewsRouter from './routes/views.router.js'
 
 const app = express()
 
@@ -11,8 +16,27 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname+'public'))
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
+app.use(
+    session({
+      store: new MongoStore({
+        mongoUrl: MONGO,
+        ttl: 40,
+      }),
+      secret: "CoderSecret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+
 app.use('/api/products', router);
 app.use('/api/carts', cartRouter)
+app.use('/views', viewsRouter)
+app.use('/api/session', sessionRouter)
 
 
 const PORT = 8080
